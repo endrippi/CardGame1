8b8fc557d389884d107a635c9a7c185112f4b807
Andreap <68429547+endrippi@users.noreply.github.com>
2026-05-23 20:37:15 +0200

    Added a reset run button

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -5,3 +5,7 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
+
+
+func _on_button_pressed() -> void:
+	get_tree().reload_current_scene()
